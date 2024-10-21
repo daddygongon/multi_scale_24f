b7f42284b5f972a0a5b0ca30f50e3b0e6e255bfa
Shigeto R. Nishiani <daddygongon@users.noreply.github.com>
2024-10-21 17:05:08 +0900

    tidy up home dir

--- w5_ruby_basic/basic_six_elements/1_puts/puts_2.rb
+++ w5_ruby_basic/basic_six_elements/1_puts/puts_2.rb
@@ -4,3 +4,13 @@
 name = 'World'            # '' 不変string
 puts "Hello #{name}."  
 puts "Hello \'#{name}\'." # '\' escape sequence
+
+=begin
+As most of the Ruby Linters
+suggest
+  use single quote literals for your strings
+and
+  go for the double ones
+    in the case of
+      interpolation/escaping sequences.
+=end
