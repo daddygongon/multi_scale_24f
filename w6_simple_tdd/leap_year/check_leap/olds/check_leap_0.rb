dfbc4162b88fac6d73d8111ddc995ff6c891d722
Shigeto R. Nishitani <nishitani@kwansei.ac.jp>
2024-10-21 17:06:23 +0900

    tidy dir

--- w6_simple_tdd/leap_year/check_leap/olds/check_leap_0.rb
+++
@@ -1,6 +0,0 @@
-#+begin_src ruby
-p year = ARGV[0].to_i
-if year%4 == 0
-  p true
-end
-#+end_src
--- w6_simple_tdd/leap_year/check_leap/olds/check_leap_1.rb
+++
@@ -1,9 +0,0 @@
-#+begin_src ruby
-p year = ARGV[0].to_i
-if year%4 == 0
-  p true
-else
-  p false
-end
-#+end_src
-
