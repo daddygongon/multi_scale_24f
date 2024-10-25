3c69b0899618655e74643a59e1dc14075bf6d90d
Shigeto R. Nishitani <nishitani@kwansei.ac.jp>
2024-10-26 04:49:38 +0900

    auto_git

---
+++ w6_redirect_pipe/gem_command_line/gem_install.sh
@@ -0,0 +1,16 @@
+> cat auto_git.rb
+puts "git add -A"
+puts "git commit"
+puts "git commit -m \'first commit\'"
+puts "git pull origin main"
+puts "git push origin main"
+> emacs auto_git.rb  #add shebang
+> mv auto_git.rb auto_git
+> chmod a+x auto_git
+
+> sudo gem install command_line
+[sudo] password for bob:
+Successfully installed command_line-2.0.1
+Parsing documentation for command_line-2.0.1
+Done installing documentation for command_line after 0 seconds
+1 gem installed
