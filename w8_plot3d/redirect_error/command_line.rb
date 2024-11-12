74c823535528b1dc87b9c4a6a253016b5f8a06fd
Shigeto R. Nishiani <daddygongon@users.noreply.github.com>
2024-11-12 15:53:23 +0900

    rev redirect_error

--- w8_plot3d/redirect_error/command_line.rb
+++ w8_plot3d/redirect_error/command_line.rb
@@ -3,6 +3,9 @@ require 'command_line/global'
 require 'colorize'
 
 comm = "ruby file_dummy_task.rb"
+# system comm
 results = command_line( comm )
 puts results.stdout.green
 puts results.stderr.red
+
+
